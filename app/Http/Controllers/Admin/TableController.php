a22f09f0da1996a4513ad746052458d91a366f3c
PedroLopes7K <pedrohenri3601@gmail.com>
2022-11-19 21:40:41 -0300

    edit and delete tables

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TableController extends Controller
 {
@@ -36,9 +38,16 @@ class TableController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'status' => $request->status,
+            'location' => $request->location,
+        ]);
+
+        return to_route('admin.tables.index');
     }
 
     /**
@@ -58,9 +67,10 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Table $table)
     {
-        //
+
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
@@ -70,9 +80,23 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Table $table)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'guest_number' => 'required',
+            'status' => 'required',
+            'location' => 'required',
+        ]);
+
+        Table::where('id', $table->id)->update([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'status' => $request->status,
+            'location' => $request->location,
+        ]);
+
+        return to_route('admin.tables.index');
     }
 
     /**
@@ -81,8 +105,10 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Table $table)
     {
-        //
+        $table->delete();
+
+        return to_route('admin.tables.index');
     }
 }
